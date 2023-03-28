0cf15818068468da5054a9d66854c7e1ac7a1a5d
ItsJustDice <ckdebruin@gmail.com>
2023-03-28 09:01:42 -0700

    get_size.c

--- get_size.c
+++ get_size.c
@@ -9,18 +9,18 @@
  */
 int get_size(const char *format, int *i)
 {
-	int curr_i = *i + 1;
+	int curr = *i + 1;
 	int size = 0;
-	
-	if (format[curr_i] == 'l')
+
+	if (format[curr] == 'l')
 		size = S_LONG;
-	else if (format[curr_i] == 'h')
+	else if (format[curr] == 'h')
 		size = S_SHORT;
 
 	if (size == 0)
-		*i = curr_i - 1;
+		*i = curr - 1;
 	else
-		*i = curr_i;
+		*i = curr;
 
 	return (size);
 }
