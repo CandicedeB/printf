ba0d75998062cad99349a64ea185410c12a87a83
ItsJustDice <ckdebruin@gmail.com>
2023-03-28 08:13:09 -0700

    get_flags.c

--- get_flags.c
+++ get_flags.c
@@ -10,7 +10,7 @@ int get_flags(const char *format, int *i)
 {
 	/* - + 0 # ' ' */
 	/* 1 2 4 8  16 */
-	int j, curr_i;
+	int i, curr_i;
 	int flags = 0;
 	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
 	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
@@ -18,17 +18,17 @@ int get_flags(const char *format, int *i)
 	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
 	{
 		/* Loop through the flag characters */
-		for (j = 0; FLAGS_CH[j] != '\0'; j++)
+		for (i = 0; FLAGS_CH[i] != '\0'; i++)
 		{
 			/* If current character match flag character, set corresponding flag bit */
-			if (format[curr_i] == FLAGS_CH[j])
+			if (format[curr_i] == FLAGS_CH[i])
 			{
 				flags |= FLAGS_ARR[j]; /* Set flag bit using bitwise OR */
 				break;
 			}
 		}
 		/* If current character does not match any flag character, break loop */
-		if (FLAGS_CH[j] == 0)
+		if (FLAGS_CH[i] == 0)
 			break;
 	}
 
