8a87f00338c535c6a65179381a3b839f1eb49ed1
nwhator <nwhator@gmail.com>
2023-03-28 17:16:32 +0100

    Updated code, added comments to handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -13,8 +13,10 @@
  */
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	int flags, int width, int precision, int size)
+
 {
 	int i, unknow_len = 0, printed_chars = -1;
+	/* Define an array of format types and their respective functions */
 	fmt_t fmt_types[] = {
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
@@ -22,18 +24,21 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
 		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
 	};
+	/* Loop through each format type to find a matching one */
 	for (i = 0; fmt_types[i].fmt != '\0'; i++)
 		if (fmt[*ind] == fmt_types[i].fmt)
+			/* If found, call corresponding print function and return its result */
 			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
-
+	/* If no matching format type is found, handle unknown formats */
 	if (fmt_types[i].fmt == '\0')
 	{
 		if (fmt[*ind] == '\0')
 			return (-1);
+		/* Print '%%' if the current character is not a valid format specifier */
 		unknow_len += write(1, "%%", 1);
-		if (fmt[*ind - 1] == ' ')
+		if (fmt[*ind - 1] == ' ') /* If previous character was a space, print */
 			unknow_len += write(1, " ", 1);
-		else if (width)
+		else if (width) /* If width skip to start of the current format specifier */
 		{
 			--(*ind);
 			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
@@ -41,7 +46,7 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 			if (fmt[*ind] == ' ')
 				--(*ind);
 			return (1);
-		}
+		} /* Otherwise, print the current character as is */
 		unknow_len += write(1, &fmt[*ind], 1);
 		return (unknow_len);
 	}
